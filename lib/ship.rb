1c1b973d8927c0b8d06e075308bab5b30bd189c5
Martin Mercer <martinmercer1@gmail.com>
2019-03-20 13:38:45 -0600

    added tests and code for innitial states for ship class

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,8 +1,16 @@
 class Ship
 
+  attr_reader :name,
+              :length,
+              :health
+
+
   def initialize(name, length)
     @name = name
     @length = length
+    @health = length
   end
+
+
 
 end
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -10,7 +10,15 @@ class ShipTest < MiniTest::Test
   end
 
   def test_it_exists
-    assert_instance_of Ship, cruiser
+    assert_instance_of Ship, @cruiser
   end
+
+  def test_it_starts_with_initial_conditions
+    assert_equal "Cruiser", @cruiser.name
+    assert_equal 3, @cruiser.length
+    assert_equal 3, @cruiser.health
+  end
+
+  
 
 end
